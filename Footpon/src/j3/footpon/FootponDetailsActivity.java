bde8fa09607e5034b9db59080a78de5ebc0db93d
Rafe <daizenga@gmail.com>
2010-10-22 13:17:24 -0400

    add draft detail view

--- Footpon/src/j3/footpon/FootponDetailsActivity.java
+++ Footpon/src/j3/footpon/FootponDetailsActivity.java
@@ -1,13 +1,32 @@
 package j3.footpon;
 
+import j3.footpon.model.Footpon;
+import j3.footpon.model.FootponRepository;
+
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FootponDetailsActivity extends Activity {
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.footpon_details);
+        
+        ArrayList<Footpon> footpons = new ArrayList<Footpon>();
+        footpons = FootponRepository.getFootponsInArea(1, 0, 0, 100);
+        TextView storeName = (TextView) findViewById(R.id.details_store_name);
+        TextView description = (TextView) findViewById(R.id.details_description);
+        TextView pointsRequired = (TextView) findViewById(R.id.details_pointsRequired);
+        
+        Footpon fp = footpons.get(0);
+        
+        storeName.setText(fp.getStoreName());
+        description.setText(fp.getDescription());
+        pointsRequired.setText("Points:" + fp.getPointsRequired());
+        
 	}
 	
 }
--- Footpon/src/j3/footpon/FootponListActivity.java
+++ Footpon/src/j3/footpon/FootponListActivity.java
@@ -6,11 +6,16 @@ import j3.footpon.model.FootponRepository;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class FootponListActivity extends Activity {
 	
+	FootponListActivity footponListActivity = this;
 	ListView listView;
 	ArrayList<Footpon> footpons;
 	public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,19 @@ public class FootponListActivity extends Activity {
 			listView = (ListView) findViewById(R.id.footponlist);
 			listView.setTextFilterEnabled(true);
 			listView.setAdapter(new FootponAdapter(this, R.layout.footpon_listitem, footpons));
-	
+			listView.setOnItemClickListener(detailsListener);
 	}
+	
+	public OnItemClickListener detailsListener = new OnItemClickListener() {
+		public void onItemClick(AdapterView<?> parent, View view,
+	            int position, long id) {
+	    	
+	    	//Footpon fp = footpons.get(position);
+	    	
+	    	Intent i = new Intent(footponListActivity,FootponDetailsActivity.class);
+
+	    	startActivity(i);
+	    }
+	};
+	
 }
