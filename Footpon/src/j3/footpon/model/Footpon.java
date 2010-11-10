2ce1239d5d0b27f5aba810a36ddb7693b27357c8
Jacky <jli.86@hotmail.com>
2010-11-10 00:59:55 -0500

    I have fixed the hard coding. It is now dynamic. The only hard coded part left is the one where the longitude and latitude are sent to the database.
    The application might crash if there is nothing on the screen and the screen is tapped.
    
    The application also now receives the barcode to generate with the coupon and the category of the coupon for searching and competitive coupons.
    
    Signed-off-by: Jacky <jli.86@hotmail.com>

--- Footpon/src/j3/footpon/model/Footpon.java
+++ Footpon/src/j3/footpon/model/Footpon.java
@@ -6,14 +6,18 @@ import org.json.JSONObject;
 public class Footpon {
 	
 	private String storeName;
+	private long code;
+	private String category;
 	private String hiddenDescription;
 	private String realDescription;
 	private double latitude;
 	private double longitude;
 	private int pointsRequired;
 	
-	public Footpon(String storeName, String hiddenDescription, String realDescription, double latitude, double longtitude,int pointsRequired){
+	public Footpon(String storeName, long code, String category, String hiddenDescription, String realDescription, double latitude, double longtitude,int pointsRequired){
 		this.storeName = storeName;
+		this.code=code;
+		this.category=category;
 		this.hiddenDescription = hiddenDescription;
 		this.realDescription = realDescription;
 		this.latitude = latitude;
@@ -40,6 +44,22 @@ public class Footpon {
 		this.storeName = storeName;
 	}
 
+	public void setCode(long code) {
+		this.code = code;
+	}
+
+	public long getCode() {
+		return code;
+	}	
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+	
 	public void setHiddenDescription(String hiddenDescription) {
 		this.hiddenDescription = hiddenDescription;
 	}
@@ -55,7 +75,7 @@ public class Footpon {
 	public String getRealDescription() {
 		return realDescription;
 	}
-
+	
 	public void setLatitude(double latitude) {
 		this.latitude = latitude;
 	}
