88940978b3276c35fb4ab92a9b816b76e8a333bc
rsheed05 <jiangtangjt@gmail.com>
2010-11-12 01:35:05 -0500

    same as last time

--- Footpon/src/j3/footpon/FootponItemizedOverlay.java
+++ Footpon/src/j3/footpon/FootponItemizedOverlay.java
@@ -1,68 +1,73 @@
 package j3.footpon;
 
 import j3.footpon.model.Footpon;
-import j3.footpon.model.FootponRepository;
+import j3.footpon.model.FootponServiceFactory;
+import j3.footpon.model.IFootponService;
 
 import java.util.ArrayList;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
-import android.sax.StartElementListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.OverlayItem;
 
-public class FootponItemizedOverlay extends ItemizedOverlay {
-
-private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+public class FootponItemizedOverlay extends ItemizedOverlay<OverlayItem>
+{
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
 	
-	public FootponItemizedOverlay(Drawable defaultMarker) {
+	public FootponItemizedOverlay(Drawable defaultMarker) 
+	{
 		  super(boundCenterBottom(defaultMarker));
 	}
 
 	Context mContext;
 	
-	public FootponItemizedOverlay(Drawable defaultMarker, Context context) {
+	public FootponItemizedOverlay(Drawable defaultMarker, Context context) 
+	{
 		  super(boundCenterBottom(defaultMarker));
 		  mContext = context;
 	}
 
 	@Override
-	protected boolean onTap(int index) {
-	  OverlayItem item = mOverlays.get(index);
+	protected boolean onTap(final int index) 
+	{
+		OverlayItem item = mOverlays.get(index);
 	  
-	  //Toast t = Toast.makeText(mContext, item.getTitle() + " \n" + item.getSnippet() , Toast.LENGTH_LONG);
-	  //t.show();
-	  Dialog dialog = new Dialog(mContext);
-	  dialog.setTitle(item.getTitle());
-	  dialog.setContentView(R.layout.footpon_dialog);
-	  Footpon fp = FootponRepository.getFootponsInArea(1, 1, 1, 1).get(index);
-	  
-	  TextView storeName = (TextView) dialog.findViewById(R.id.dialog_store_name);
-      TextView description = (TextView) dialog.findViewById(R.id.dialog_description);
-      TextView pointsRequired = (TextView) dialog.findViewById(R.id.dialog_pointsRequired);
-      Button detailsButton = (Button) dialog.findViewById(R.id.dialog_show_details);
-      
-      detailsButton.setOnClickListener(new Button.OnClickListener(){
-
-		@Override
-		public void onClick(View v) {
-			//
-		}
+		//Toast t = Toast.makeText(mContext, item.getTitle() + " \n" + item.getSnippet() , Toast.LENGTH_LONG);
+		//t.show();
+		Dialog dialog = new Dialog(mContext);
+		dialog.setTitle(item.getTitle());
+		dialog.setContentView(R.layout.footpon_dialog);
 		
-      });
-      storeName.setText(fp.getStoreName());
-      description.setText(fp.getDescription());
-      pointsRequired.setText("Points:" + fp.getPointsRequired());
+		IFootponService service = FootponServiceFactory.getService();
+		Footpon fp = service.getInstance().get(index);
 	  
-      
+		TextView storeName = (TextView) dialog.findViewById(R.id.dialog_store_name);
+		TextView hiddenDescription = (TextView) dialog.findViewById(R.id.dialog_hiddenDescription);
+		TextView pointsRequired = (TextView) dialog.findViewById(R.id.dialog_pointsRequired);
+		Button detailsButton = (Button) dialog.findViewById(R.id.dialog_show_details);
+        
+		detailsButton.setOnClickListener(new Button.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Intent i = new Intent(mContext, Coupon.class);
+				i.putExtra("index", index);
+				mContext.startActivity(i);
+			}
+		}
+		);
+		
+      storeName.setText(fp.getStoreName());
+      hiddenDescription.setText(fp.getHiddenDescription());
+      pointsRequired.setText("Points:" + fp.getPointsRequired());
       
 	  dialog.show();
 	  
