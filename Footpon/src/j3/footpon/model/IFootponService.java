d2090af9d60fd5c77f6edb416f682ed0eef1b4df
Rafe <daizenga@gmail.com>
2010-11-19 23:59:04 -0500

    add interface for footpon service

--- Footpon/src/j3/footpon/model/IFootponService.java
+++ Footpon/src/j3/footpon/model/IFootponService.java
@@ -2,7 +2,16 @@ package j3.footpon.model;
 
 import java.util.ArrayList;
 
+
+//interface for Footpon Service
 public interface IFootponService {
 	public ArrayList<Footpon> getFootponsInArea(double longtitude,double latitude);
+
+	//return service instance, it's a singleton
 	public ArrayList<Footpon> getInstance();
+	public ArrayList<Footpon> getMyFootpons(int userId);
+	public boolean redeemFootpon(int userId,int footponId);
+	public boolean useFootpon(int userId,int footponId);
+	public boolean sync(int point);
+	
 }
