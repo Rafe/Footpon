62e121f82d7a46c1fe69c0dc20fd231b08ee3f3e
Rafe <daizenga@gmail.com>
2010-11-26 14:12:50 -0500

    change details view

--- Footpon/src/j3/footpon/FootponDetailsActivity.java
+++ Footpon/src/j3/footpon/FootponDetailsActivity.java
@@ -4,6 +4,7 @@ import j3.footpon.model.Footpon;
 import j3.footpon.model.FootponServiceFactory;
 import j3.footpon.model.IFootponService;
 import j3.footpon.model.IconHelper;
+import j3.footpon.pedometer.StepService;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -14,13 +15,14 @@ import android.widget.Toast;
 
 public class FootponDetailsActivity extends Activity {
 	private TextView storeName;
-	private TextView rdescription;
-	private TextView hdescription;
+	private TextView description;
 	private TextView stepsRequired;
 	private TextView startDate;
 	private TextView expireDate;
 	private ImageView logo;
+	private TextView steps;
 	IFootponService service;
+	
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -53,23 +55,23 @@ public class FootponDetailsActivity extends Activity {
 	
 	private void getView(){
 		storeName = (TextView) findViewById(R.id.detail_store_name);
-		rdescription = (TextView) findViewById(R.id.detail_real_description);
-		hdescription = (TextView) findViewById(R.id.detail_hidden_description);
+		description = (TextView) findViewById(R.id.detail_description);
 		stepsRequired = (TextView) findViewById(R.id.detail_required_steps);
 		startDate = (TextView) findViewById(R.id.detail_start_date);
 		expireDate = (TextView) findViewById(R.id.detail_expire_date);
 		logo = (ImageView) findViewById(R.id.logo);
+		steps = (TextView) findViewById(R.id.detail_steps);
 	}
 	
 	private void setView(Footpon footpon) {
 		if(footpon != null){
 			storeName.setText(footpon.getStoreName());
-			rdescription.setText(footpon.getRealDescription());
-			hdescription.setText(footpon.getHiddenDescription());
+			description.setText(footpon.getRealDescription());
 			startDate.setText(footpon.getStartDate());
 			expireDate.setText(footpon.getEndDate());
 			stepsRequired.setText(Long.toString(footpon.getStepsRequired()));
 			logo.setImageDrawable(IconHelper.getLogo(footpon.getStoreName(), this));
+			steps.setText(String.valueOf(StepService.steps));
 		}
 	}
 }
