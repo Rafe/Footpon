9ceeaf507f3ced17550ca39ae9c00483aabadb6c
Jacky <jli.86@hotmail.com>
2010-12-03 13:22:19 -0500

    Fixed bug in list activity.
    Signed-off-by: Jacky <jli.86@hotmail.com>

--- Footpon/src/j3/footpon/FootponListActivity.java
+++ Footpon/src/j3/footpon/FootponListActivity.java
@@ -1,6 +1,7 @@
 package j3.footpon;
 
 import j3.footpon.model.Footpon;
+import j3.footpon.model.FootponService;
 import j3.footpon.model.FootponServiceFactory;
 import j3.footpon.model.User;
 
@@ -26,6 +27,7 @@ public class FootponListActivity extends Activity {
 	ArrayList<Footpon> footpons = null;
 	FootponAdapter adapter;
 	String username;
+	ArrayList<Long> IDs = new ArrayList<Long>();
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -57,6 +59,10 @@ public class FootponListActivity extends Activity {
 		if(footpons != null) {
 			adapter = new FootponAdapter(this, R.layout.footpon_listitem, footpons);
 			
+			for(int i=0;i<footpons.size();i++) {
+				IDs.add(footpons.get(i).getID());
+				}
+			
 			listView = (ListView) findViewById(R.id.footponlist);
 			listView.setTextFilterEnabled(true);
 			listView.setAdapter(adapter);
@@ -64,13 +70,16 @@ public class FootponListActivity extends Activity {
 		}
 	}
 	
-
 	public OnItemClickListener detailsListener = new OnItemClickListener() {
 		public void onItemClick(AdapterView<?> parent, View view,
 	            int position, long id) {
+			SharedPreferences share = getSharedPreferences(User.SHARE_USER_INF_TAG, 0);
+			username = share.getString(User.SHARE_USERNAME, "");
+			
 	    	Intent i = new Intent(footponListActivity, FootponDetailsActivity.class);
 	    	Bundle bundle=new Bundle();
-	    	bundle.putLong("id", footpons.get(position).getID());
+	    	//bundle.putLong("id", id);
+	    	bundle.putLong("id", IDs.get(position));
 	    	//FootponServiceFactory.getService().redeemFootpon(username, footpons.get(position).getID());
 	    	bundle.putBoolean("own",true);
 	    	i.putExtras(bundle);
